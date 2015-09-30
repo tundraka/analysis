7f350baec8a9ceff647928b5c3df964de313498e
tundraka <tundraka@gmail.com>
2015-09-29 23:22:46 -0500

    ColClasses.

--- vaers/vaers.R
+++ vaers/vaers.R
@@ -1,28 +1,15 @@
 library(data.table)
 
+vaersDataFile <- '../data/VAERS/2014/2014VAERSDATA.CSV'
+vaersDataFile <- '2014VAERSDATA.CSV'
+datesAs <- 'character'
+colClasses <- c('numeric', datesAs, 'factor', rep('numeric', 3), 'factor', datesAs,
+                'character', 'factor', datesAs, rep('factor', 3), 'numeric',
+                rep('factor', 3), rep(datesAs, 2), 'numeric', 'character',
+                rep('factor', 2), rep('character', 5))
+
+# fread is reading string fields that contain a , as different fields, looks like
+# this is a know issue.
+vaersdata <- as.data.table(read.csv(vaersDataFile, colClasses=colClasses))
+
 # TODO. The Date fields are not detected.
-colClasses <- c('numeric', 'date', 'factor', rep('numeric', 3), 'factor', 'date',
-                'character', 'logical', 'date', rep('logical', 3), 'numeric',
-                rep('logical', 3)
-vaersdata <- fread('../data/VAERS/2014/2014VAERSDATA.CSV')
-vaersdata <- rename(vaersdata,
-                    ReportDate = RECVDATE,
-                    State = STATE,
-                    Age = AGE_YRS,
-                    Years = CAGE_YR,
-                    Months = CAGE_MO)
-
-# Or without dplyr
-# names(vaersdata)[1] <- "ReportDate"
-# names(vaersdata)[2] <- "State"
-# names(vaersdata)[3] <- "Age"
-# etc.
-
-# Some commands
-# head
-# summary(vaersdata$STATE)
-# tail
-# str
-# dim
-#
-
