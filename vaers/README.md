98c4ae9cb5932e75ae78c8a6e23ccebf37beac38
tundraka <tundraka@gmail.com>
2015-10-07 12:07:15 -0500

    Adding readme and starting to explore more of the VAERS data.

---
+++ vaers/README.md
@@ -0,0 +1,34 @@
+#VAERS (Vaccine Adverser Event Reporting System) Data
+The VAERS data can be downloaded from [here][1]. The VAERS website has some
+diclaimers about the data, where it's coming from, and to be careful what you
+deduce from it.
+
+I think the VAERS data doesn't offer the whole picture since this represent the
+report from the pacient, but we never know what the CDC did with that report,
+what investigation, outcome, etc.
+
+The VAERS data is divided in several files, 1 for each year and can normally be
+found under the [data page][2]. In order to download the files, we will be
+challenged by a captcha. The data URL follow a pattern like:
+
+```
+https://vaers.hhs.gov/eSubDownload/index.jsp?fn={xxxx}VAERSData.zip
+# Where xxxx is the year 1990 - present
+```
+
+##Open questions to the VAERS staff
+- What percentage of the reports are investigated by VAERS?
+- Is there an initial vetting mechanism to determine if VAERS gets involved and
+  request more information about a specific event?
+- Is it possible to access the outcome of the investigation that VAERS did after
+  a report has been investigated?
+- Is it possible to get more information about the event, specifically about the
+  demographics? Like ethnicity, income, practice, etc.
+
+####VAERS Contact info.
+For more information, please call the VAERS Information Line toll-free at
+(800) 822-7967 or e-mail to info@vaers.org.
+
+[1]: https://vaers.hhs.gov/data/index
+[2]: https://vaers.hhs.gov/data/data
+
--- vaers/vaers.R
+++ vaers/vaers.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(stringr)
+library(ggplot2)
 
 vaersDataFile <- 'data/VAERS/2014/2014VAERSDATA.CSV'
 #vaersDataFile <- '2014VAERSDATA.CSV'
@@ -9,13 +10,52 @@ colClasses <- c('numeric', datesAs, 'factor', rep('numeric', 3), 'factor', dates
                 rep('factor', 3), rep(datesAs, 2), 'numeric', 'character',
                 rep('factor', 2), rep('character', 5))
 
+colNames <- c('vaersid', 'datereceived', , 'state', 'age', 'ageyears', 'agemonths',
+              'sex', 'reportdate', 'symptoms', 'died', 'datedied',
+              'lifethreateningeffect', 'ervisit', 'hospitalized',
+              'hospitalizeddays', 'prolongedhospitalization', 'disable',
+              'recovered', 
+
+# According to the VAERS code book, the age fields represent:
+#
+# The values for this variable range from 0 to <1. It is only calculated for
+# patients age 2 years or less. The variables CAGE_YR and CAGE_MO work in
+# conjunction to specify the calculated age of a person. For example, if
+# CAGE_YR=1 and CAGE_MO=.5 then the age of the individual is 1.5 years or 1 year
+# 6 months.
+ageDifference <- vaersdata[(age != (ageyears + agemonths)), .N]
+if (ageDifference > 0) {
+    print('The reported age in "age" and the sum of "ageyears" + "agemonths" is')
+    print(paste('different in:', ageDifference, 'records'))
+}
+
 # fread is reading string fields that contain a , as different fields, looks like
 # this is a know issue.
 vaersdata <- as.data.table(read.csv(vaersDataFile, colClasses=colClasses))
+
+# TODO set better column names.
 currentColNames <- names(vaersdata)
 setnames(vaersdata, currentColNames, tolower(gsub('_', '', currentColNames)))
 
 totsByState <- vaersdata[,.(tot=.N), .(state)][order(-tot)]
-barplot(totsByState$tot)
 
-totsBySexState <- vaersdata[,.(tot=.N), .(state, sex)][order(-tot, state)]
+#ggplot(totsByState[1:20], aes(state, weight=tot)) +
+       #geom_bar() +
+       #labs(title='2014 VAERS: Reports by top 20 states')
+ggplot(totsByState[1:20], aes(state, tot)) +
+       geom_bar(stat='identity') +
+       labs(title='2014 VAERS: Reports by top 20 states')
+
+
+# Let's select only the top 10 states.
+# TODO. I need to order based on the total. Right now it's ordering by the state
+# and sex since the total by state is divided between male/female/unknown. Probably
+# what I'll need to do is some melt/dcast
+totsBySexState <- vaersdata[state==totsByState[1:10, .(state)]$state,
+                            .(tot=.N),
+                            .(state, sex)][order(state, sex)]
+
+ggplot(totsBySexState, aes(state, tot, fill=sex)) +
+    geom_bar(stat='identity', position='dodge') +
+    labs(title='2014 VAERS: Top 10 states with more reports')
+    
