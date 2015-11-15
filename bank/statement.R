87f9a49727a9a1e1a78dba105da0020d9cd8e490
tundraka <tundraka@gmail.com>
2015-11-14 22:30:51 -0600

    Frustration.

--- bank/statement.R
+++ bank/statement.R
@@ -7,6 +7,13 @@ source('bank/transactions.R')
 library(ggplot2)
 
 # TODO Load the list of time periods
+# Event though I try to make this generic, like specifyng where the data is, what
+# are the file naming conventions, seems to me that the R code is linked so
+# closely to the data and formats of the files that will read. For example, I set
+# a conf for the data paths following a specific pattern. Then I can say for those
+# files, read the columns X, Y, Z, and so on keep adding specifics, but a generic
+# way cannot be defined, unless I impose that a pattern, put things here, name
+# them like this, make sure this column represents this, and so on.
 statementLoader <- statement(timePeriod)
 transactions <- statementLoader$readStatements()
 
