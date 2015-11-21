768c7c1f4e2deee9481e72b3d053ee7521aaa260
tundraka <tundraka@gmail.com>
2015-11-21 01:53:02 -0600

    Simpligying statement reading.

--- bank/statement.R
+++ bank/statement.R
@@ -15,45 +15,63 @@ library(ggplot2)
 statementLoader <- statement()
 classifier <- classification()
 
-files <- list(
+fileTypeConf <- list(
     DEBIT = list(
         cols=c(rep('character', 3), 'numeric', rep('character', 4)),
         sel=1:4),
     CC1 = list(
-        cols=c(rep('character', 4), 'numeric')
+        cols=c(rep('character', 4), 'numeric'),
         sel=c(1, 2, 4, 5)),
     CC2 = list(
-        cols=c(rep('character', 4), 'numeric')
+        cols=c(rep('character', 4), 'numeric'),
         sel=c(1, 2, 4, 5))
     )
 
-transactions <- statementLoader$readStatements('201510')
-transactions <- classifier$classify(transactions)
+readTransactions <- function(fileName) {
+    trx <- data.table()
+    fileNameParts <- unlist(strsplit(basename(fileName), "\\."))
+    if (length(fileNameParts) == 2) {
+        acct <- fileNameParts[1]
+        conf <- fileTypeConf[[acct]]
+        trx <- statementLoader$readStatements(fileName, conf, acct)
+    } else {
+        print(paste('unable to read file', fileName))
+    }
 
-# list.files('data/bank', pattern="*.csv", all.files = TRUE, full.names = TRUE)
+    trx
+}
+files <- list.files('data/bank/2015/08', pattern="*.csv", all.files=T,
+                    full.names=T)
+transactions <- data.table()
+tranasctions <- lapply(files, readTransactions)
+
+#transactions <- statementLoader$readStatements('201510')
+#transactions <- classifier$classify(transactions)
+
+# 
 #
 # Explore
 #
-sales <- transactions[type=='sale',
-                      .(description, category, name, amount=amount*-1, day)]
+#sales <- transactions[type=='sale',
+                      #.(description, category, name, amount=amount*-1, day)]
 
-sales[, .(tot=sum(amount)), .(category)][order(-tot)]
-sales[, .(tot=sum(amount), visits=.N), .(name, category)
-      ][order(category, -tot)]
+#sales[, .(tot=sum(amount)), .(category)][order(-tot)]
+#sales[, .(tot=sum(amount), visits=.N), .(name, category)
+      #][order(category, -tot)]
 
-# Some specifics.
-printCategory <- function(cat) {
-    sales[category==cat, .(tot=sum(amount), visits=.N),
-                 .(name)][,.(name, tot, visits,
-                             totpvisit=round(tot/visits, digits=2))][
-                 order(-tot, visits)]
-}
+## Some specifics.
+#printCategory <- function(cat) {
+    #sales[category==cat, .(tot=sum(amount), visits=.N),
+                 #.(name)][,.(name, tot, visits,
+                             #totpvisit=round(tot/visits, digits=2))][
+                 #order(-tot, visits)]
+#}
 
-cats <- c('groceries', 'restaurant')
-lapply(cats, printCategory)
+#cats <- c('groceries', 'restaurant')
+#lapply(cats, printCategory)
 
-# Draw a boxplot for each day and category
-ggplot(sales, aes(day, amount)) + geom_boxplot()
-ggplot(sales, aes(category, amount)) + geom_boxplot()
-ggplot(sales, aes(x=day, y=category)) + geom_bin2d() +
-    scale_fill_gradient(low="green", high="red")
+## Draw a boxplot for each day and category
+#ggplot(sales, aes(day, amount)) + geom_boxplot()
+#ggplot(sales, aes(category, amount)) + geom_boxplot()
+#ggplot(sales, aes(x=day, y=category)) + geom_bin2d() +
+    #scale_fill_gradient(low="green", high="red")
--- bank/transactions.R
+++ bank/transactions.R
@@ -4,60 +4,21 @@ library(data.table)
 statement <- function() {
     dateFormat = '%m/%d/%Y'
 
-    fileName <- function(timePeriod, acctFile) {
-        configuration <- list(
-            filePath = 'data/bank/',
-            filePrefix = paste0(timePeriod, '-'),
-            extension = '.csv'
-        )
+    readStatements <- function(fileName, conf, acct) {
 
-        paste0(
-               configuration$filePath,
-               configuration$filePrefix,
-               acctFile,
-               configuration$extension)
-    }
+        transactions <- fread(fileName, header=T, colClasses=conf$cols, select=conf$sel)
 
-    readStatements <- function(timePeriod) {
-
-        readFile <- function(fileName, classes, cols, label) {
-            data <- fread(fileName, header=T, colClasses=classes, select=cols)
-            data[,account:=label]
-
-            data
-        }
-
-        # Col names/classes for the CC/debit statements
-        debClasses <- c(rep('character', 3), 'numeric', rep('character', 4))
-        ccClasses <- c(rep('character', 4), 'numeric');
-
-        # The col selected are:
-        # 1: Type
-        # 2: Transaction date
-        # 4: Description
-        # 5: Amount
-        ccColSelected <- c(1, 2, 4, 5)
-
-        cc1 <- readFile(fileName(timePeriod, 1), ccClasses, ccColSelected, 'CC1')
-        cc2 <- readFile(fileName(timePeriod, 2), ccClasses, ccColSelected, 'CC2')
-        debit <- readFile(fileName(timePeriod, 3), debClasses, 1:4, 'DEBIT')
-
-        transactions <- rbindlist(list(cc1, cc2, debit))
-
-        # Removing temp variables.
-        remove(cc1, cc2, debit)
-
-        colNames<- c('type', 'date', 'description', 'amount', 'account')
+        colNames<- c('type', 'date', 'description', 'amount')
         setnames(transactions, names(transactions), colNames)
         transactions[,`:=`(description=gsub(' +', ' ', description),
                            date=as.Date(date, format=dateFormat),
-                           type=as.factor(tolower(type)))]
+                           type=as.factor(tolower(type)),
+                           account=acct)]
 
         return(transactions)
     }
 
     list(
-         fileName=fileName,
          readStatements=readStatements
     )
 }
