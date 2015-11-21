aa6e45cfbfacf7ec6358ab981256b10a20aeb01b
tundraka <tundraka@gmail.com>
2015-11-21 01:55:10 -0600

    Removing unused file.

--- bank/move.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-files=$(ls -1 csv)
-for file in "${files[@]}"
-do
-    fileOnly=$(basename $file)
-    fileName=$(cut fileOnly
-    echo $fileOnly | sed "s/\([0-9]\{4\}\)\([0-9]\{2\}\)-\([0-9]\)\.csv/\1-\2-\3/"
-done
